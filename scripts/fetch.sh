66079e27b58f110e093623240a80a53d02382d26
Chris McCormick <chrism@anaplian>
2009-05-21 20:41:04 +0100

    Initial import.

---
+++ scripts/fetch.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget http://github.com/jeresig/processing-js/raw/master/processing.min.js -O js/processing.min.js
+wget http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js -O js/jquery.js
