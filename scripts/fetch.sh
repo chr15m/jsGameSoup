70ad1ffb7385da704808027a2efc56ae9ecf4796
chrism <chrism@byzo.local>
2009-05-22 15:45:34 +0100

    Make sure useful stuff in init.js isn't overwritten.

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -2,5 +2,5 @@
 
 wget http://github.com/jeresig/processing-js/raw/master/processing.js -O js/processing.js
 #wget http://github.com/jeresig/processing-js/raw/master/processing.min.js -O js/processing.js
-wget http://github.com/jeresig/processing-js/raw/master/examples/init.js -O js/init.js
-wget http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js -O js/jquery.js
+#wget http://github.com/jeresig/processing-js/raw/master/examples/init.js -O js/init.js
+#wget http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js -O js/jquery.js
