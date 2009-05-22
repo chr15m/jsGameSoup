7b6e7291ee5998de84900eb7c39e39544088b9a8
chrism <chrism@byzo.local>
2009-05-22 15:31:17 +0100

    Integrating processing.

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-wget http://github.com/jeresig/processing-js/raw/master/processing.min.js -O js/processing.min.js
+wget http://github.com/jeresig/processing-js/raw/master/processing.js -O js/processing.js
+#wget http://github.com/jeresig/processing-js/raw/master/processing.min.js -O js/processing.js
+wget http://github.com/jeresig/processing-js/raw/master/examples/init.js -O js/init.js
 wget http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js -O js/jquery.js
