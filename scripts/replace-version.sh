1ef1e5a1cf41742ff4ea3eebb1b87f7191ad51f6
chrism <chrism@byzo.local>
2009-07-13 23:11:52 +0100

    let us try this fix on the precommit

--- scripts/replace-version.sh
+++ scripts/replace-version.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-bzrrev=`bzr log -l 1 --line | cut -f1 -d":"`; sed "s/v[0-9][0-9]*/v$bzrrev/g" README
+bzrrev=`bzr log -l 1 --line | cut -f1 -d":" | xargs expr 1 +`; sed "s/v[0-9][0-9]*/v$bzrrev/g" README
