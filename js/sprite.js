0712beb01eb677f13334e5ea3e1182916027e8b5
Chris McCormick <chris@mccormick.cx>
2010-10-31 23:26:59 +0800

    new animated sprite class which works well with the canvas.

--- js/sprite.js
+++ js/sprite.js
@@ -1,75 +1,52 @@
-var spriteids = 0;
-
-/* sprite class - controls a single image on the screen */
-function Sprite(url, parentdiv, hasheight, frames) {
-	// create a unique id
-	spriteids += 1;
-	var id = spriteids;
+/* sprite class - controls a set of images on the screen with different 'action' animations */
+function Sprite(anchor, frames, loadedcallback) {
+	var loadcount = 0;
+	var action = "";
+	var framecount = -1;
+	var frame = 0;
+	var sprite = this;
 	
-	// internal variables
-	this.pos = [0, 0];
-	this.loaded = false;
-	this.heightoffset = !hasheight + 1;
-	if (frames)
-		this.frames = frames;
-	else
-		this.frames = 1;
-	this.halfwidth = 0;
-	this.halfheight = 0;
-	
-	// remember the container of our div
-	this.parentdiv = parentdiv;
-	
-	// create an empty div to hold our new sprite
-	this.div = document.createElement('div');
-	this.div.id = "sprite-" + id;
-	parentdiv.appendChild(this.div);
-	
-	// load the image
-	this.img = new Image();
-	this.img.name = url;
-	this.img.sprite = this;
-	this.img.src = url;
-	this.img.onload = function () {
-		//alert("'" + this.name + "' is " + this.width + " by " + this.height + " pixels in size.");
-		var div = this.sprite.div;
-		div.style.position = "absolute";
-		div.style.width = this.width / this.sprite.frames;
-		div.style.height = this.height;
-		this.sprite.halfwidth = (this.width / this.sprite.frames) / 2;
-		this.sprite.halfheight = this.height / this.sprite.heightoffset;
-		div.style.padding = 0;
-		div.style.margin = 0;
-		div.style.backgroundImage = "url(" + this.src + ")";
-		this.sprite.loaded = true;
-		this.sprite.update();
-		return true;
+	// load up all of the images
+	for (var a in frames) {
+		for (var f=0; f<frames[a].length; f++) {
+			loadcount += 1;
+			var img = new Image();
+			img.src = frames[a][f][0];
+			frames[a][f][0] = img;
+			img.onload = function () {
+				loadcount -= 1;
+				if (loadcount == 0) {
+					if (loadedcallback) {
+						loadedcallback();
+					}
+				}
+			}
+		}
 	}
 	
-	// update from new settings
-	this.update = function() {
-		this.div.style.left = this.pos[0] - this.halfwidth;
-		this.div.style.top = this.pos[1] - this.halfheight;
-		return this;
+	// set which action to play
+	this.action = function(a) {
+		action = a;
+		framecount = frames[a][0][1];
+		frame = 0;
+		sprite.update = sprite._update;
+		sprite.draw = sprite._draw;
 	}
 	
-	// set this sprite's z-index
-	this.z = function(z) {
-		this.div.style.zIndex = z;
-		return this;
+	// increment frame counter etc.
+	this._update = function() {
+		framecount -= 1;
+		if (framecount <= 0) {
+			frame = (frame + 1) % frames[action].length;
+			framecount = frames[action][frame][1];
+		}
 	}
 	
-	// move the sprite to a specific on-screen position
-	// this is the center of the sprite
-	this.move = function(pos) {
-		this.pos = pos;
-		if (this.loaded)
-			this.update();
-		return this;
+	// draw this sprite on canvas c at position with respect to the anchor specified
+	this._draw = function(c, x, y) {
+		c.drawImage(frames[action][frame][0], x, y);
 	}
 	
-	// delete this sprite
-	this.destroy = function() {
-		parentdiv.removeChild(this.div);
-	}
+	this.update = function() {};
+	this.draw = function() {};
 }
