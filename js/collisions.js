e11487274ccd64f3ef90036c71c6d2821bb3aab8
Chris McCormick <chris@mccormick.cx>
2010-11-04 23:06:20 +0800

    If poly is completely inside other poly then trigger collision.

--- js/collisions.js
+++ js/collisions.js
@@ -3,8 +3,8 @@
 collide = {}
 
 // generic function for running fn on all of groupa and groupb
-collide.collideall = function(fn, type) {
-	var fnname = "collide_" + type;
+collide.collideall = function(fn, calltype) {
+	var fnname = "collide_" + calltype;
 	return function(groupa, groupb) {
 		for (var a in groupa) {
 			for (var b in groupb) {
@@ -14,9 +14,9 @@ collide.collideall = function(fn, type) {
 					var collisionresult = fn(ae, be);
 					if (collisionresult) {
 						if (ae[fnname])
-							ae[fnname](be, type, collisionresult);
+							ae[fnname](be, collisionresult);
 						if (be[fnname])
-							be[fnname](ae, type, collisionresult);
+							be[fnname](ae, collisionresult);
 					}
 				}
 			}
@@ -54,75 +54,82 @@ collide.collide_circle_entities = function(a, b) {
 
 collide.circles = collide.collideall(collide.collide_circle_entities, "circle");
 
-// TODO: uncomment poly collisions below
+/* Detect whether a point is inside a polygon (list of points) or not. This code is annoyingly duplicated inside jsgamesoup too. */
+var pointInPoly = function(pos, poly) {
+	/* This code is patterned after [Franklin, 2000]
+	http://www.geometryalgorithms.com/Archive/algorithm_0103/algorithm_0103.htm
+	Tells us if the point is in this polygon */
+	cn = 0
+	pts = poly.slice();
+	pts.push([poly[0][0], poly[0][1]]);
+	for (var i=0; i<poly.length; i++)
+		if (((pts[i][1] <= pos[1]) && (pts[i+1][1] > pos[1])) || ((pts[i][1] > pos[1]) && (pts[i+1][1] <= pos[1])))
+			if (pos[0] < pts[i][0] + (pos[1] - pts[i][1]) / (pts[i+1][1] - pts[i][1]) * (pts[i+1][0] - pts[i][0]))
+	cn += 1
+	return cn % 2
+}
 
-/*
-	this.lineOnLine = function lineOnLine(l1, l2) {
-		// Detects the intersection of two lines
-		//   http://www.kevlindev.com/gui/math/intersection/Intersection.js
-		var a1 = l1[0];
-		var a2 = l1[1];
-		var b1 = l2[0];
-		var b2 = l2[1];
-		var a1x = a1[0];
-		var a1y = a1[1];
-		var a2x = a2[0];
-		var a2y = a2[1];
-		var b1x = b1[0];
-		var b1y = b1[1];
-		var b2x = b2[0];
-		var b2y = b2[1];
+// helper function
+var lineOnLine = function(l1, l2) {
+	// Detects the intersection of two lines
+	//   http://www.kevlindev.com/gui/math/intersection/Intersection.js
+	var a1 = l1[0];
+	var a2 = l1[1];
+	var b1 = l2[0];
+	var b2 = l2[1];
+	var a1x = a1[0];
+	var a1y = a1[1];
+	var a2x = a2[0];
+	var a2y = a2[1];
+	var b1x = b1[0];
+	var b1y = b1[1];
+	var b2x = b2[0];
+	var b2y = b2[1];
+	
+	var ua_t = (b2x - b1x) * (a1y - b1y) - (b2y - b1y) * (a1x - b1x);
+	var ub_t = (a2x - a1x) * (a1y - b1y) - (a2y - a1y) * (a1x - b1x);
+	var u_b  = (b2y - b1y) * (a2x - a1x) - (b2x - b1x) * (a2y - a1y);
+	
+	if (u_b) {
+		var ua = ua_t / u_b;
+		var ub = ub_t / u_b;
 		
-		var ua_t = (b2x - b1x) * (a1y - b1y) - (b2y - b1y) * (a1x - b1x);
-		var ub_t = (a2x - a1x) * (a1y - b1y) - (a2y - a1y) * (a1x - b1x);
-		var u_b  = (b2y - b1y) * (a2x - a1x) - (b2x - b1x) * (a2y - a1y);
-		
-		if (u_b) {
-			var ua = ua_t / u_b;
-			var ub = ub_t / u_b;
-			
-			if (0 <= ua && ua <= 1 && 0 <= ub && ub <= 1) {
-				// intersection
-				return [a1x + ua * (a2x - a1x), a1y + ua * (a2y - a1y)];
-			} else {
-				return [];
-			}
+		if (0 <= ua && ua <= 1 && 0 <= ub && ub <= 1) {
+			// intersection
+			return [a1x + ua * (a2x - a1x), a1y + ua * (a2y - a1y)];
 		} else {
-			if (ua_t == 0 || ub_t == 0) {
-				// coincident
-				//return [line2]
-				//this will be caught elsewhere anyway
-				return [(a2x + a1x) / 2, (a2y + a1y) / 2];
-			} else {
-				// parallel
-				return [];
-			}
+			return [];
+		}
+	} else {
+		if (ua_t == 0 || ub_t == 0) {
+			// coincident
+			//return [line2]
+			//this will be caught elsewhere anyway
+			return [(a2x + a1x) / 2, (a2y + a1y) / 2];
+		} else {
+			// parallel
+			return [];
 		}
 	}
-	
-	for (var o=0; o<entitiesColliders.length; o++) {
-		for (var e=o; e<entitiesColliders.length; e++) {
-			if (e != o) {
-				if (this.collidePolyPoly(entitiesColliders[o].collisionPoly(), entitiesColliders[e].collisionPoly())) {
-					if (entitiesColliders[o].collided)
-						entitiesColliders[o].collided(entitiesColliders[e]);
-					if (entitiesColliders[e].collided)
-						entitiesColliders[e].collided(entitiesColliders[o]);
-				}
-			}
-		}
-	}
-	
-	// Test whether two polygons are touching
-	this.collidePolyPoly = function collidePolyPoly(e1, e2) {
-		var collided = false;
+}
+
+/** Test whether two polygons are touching. Entities should have a get_collision_poly() method which returns an array of lines. */
+collide.polys = function() {};
+
+collide.collide_poly_entities = function(a, b) {
+	if (b.get_collision_poly && a.get_collision_poly) {
+		var e1 = a.get_collision_poly();
+		var e2 = b.get_collision_poly();
 		for (var l1=0; l1<e1.length; l1++) {
 			for (var l2=0; l2<e2.length; l2++) {
-				if (this.lineOnLine([e1[l1], e1[(l1 + 1) % e1.length]], [e2[l2], e2[(l2 + 1) % e2.length]]).length) {
-					collided = true;
+				if (lineOnLine([e1[l1], e1[(l1 + 1) % e1.length]], [e2[l2], e2[(l2 + 1) % e2.length]]).length) {
+					return true;
 				}
 			}
 		}
-		return collided;
+		// if we get here test if a point from each poly is inside the other
+		return pointInPoly(e1[0], e2) || pointInPoly(e2[0], e1);
 	}
-*/	
+}
+
+collide.polys = collide.collideall(collide.collide_poly_entities, "poly");
