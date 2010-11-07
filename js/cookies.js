fa751e3c368e681f440028b54a6c00aad8711b3c
Chris McCormick <chris@mccormick.cx>
2010-11-07 23:40:10 +0800

    standalone module for setting, getting, and deleting cookies.

---
+++ js/cookies.js
@@ -0,0 +1,27 @@
+// adapted from: www.quirksmode.org/js/cookies.html
+
+function setCookie(name, value, days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 2000));
+		var expires = "; expires =" + date.toGMTString();
+	} else {
+		var expires = "";
+	}
+	document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(";");
+	for (var i=0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}
+
+function delCookie(name) {
+	setCookie(name, "", -1);
+}
