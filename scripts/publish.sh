5dea277d267c635156ba4cc135e5cc875e1c50b3
Chris McCormick <chris@mccormick.cx>
2011-01-22 17:11:35 +0800

    Documentation tweaks.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+make clean -C docs/
 make -C docs/
 rsync -avz docs/* chrism@mccormick.cx:~/mccormick.cx/projects/jsGameSoup/
 bzr push https://mccormix@jsgamesoup.googlecode.com/svn/jsgamesoup/
