fffb95814c53386f4905fe76b1453fb3695a666b
Chris McCormick <chris@mccormick.cx>
2011-02-03 23:03:46 +0800

    Fixed sprite preloader class to work the way it says in terms of callbacks.

--- js/sprite.js
+++ js/sprite.js
@@ -121,17 +121,21 @@ function Sprite(anchor, frames, loadedcallback) {
 	@method preload
 	@description Pre-loads a whole array of images. Provides feedback on which images are loaded via the progresscallback, which returns the number of images left to load each time one is loaded, and 0 when the final image is loaded.
 	@param images is an array of strings containing the URLs of images to load.
-	@param progresscallback is a function accepting an integer, which is the count of images left to load.
+	@param completedcallback is a function which is called when all images are loaded
+	@param progresscallback is a function accepting an integer, which is the count of images left to load
 */
-Sprite.preload = function(images, progresscallback) {
-	var loadcount = images.length - 1;
+Sprite.preload = function(images, completedcallback, progresscallback) {
+	var loadcount = images.length;
 	for (var i=0; i<images.length; i++) {
 		var img = new Image();
 		img.src = images[i];
 		img.onload = function () {
 			loadcount -= 1;
 			if (progresscallback)
-				progresscallback(images.length - loadcount - 1);
+				progresscallback(loadcount);
+			if (loadcount == 0 && completedcallback) {
+				completedcallback();
+			}
 		}
 	}
 }
