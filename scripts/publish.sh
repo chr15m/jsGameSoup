7afa215298b96b1f43d3c7c151b6b84dc25fc5b2
Chris McCormick <chris@mccormick.cx>
2011-05-26 21:48:48 +0800

    Changes to make this work for github push too.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 scripts/replace-version.sh js/jsgamesoup.js
-scripts/replace-version.sh README
+scripts/replace-version.sh README.markdown
 bzr commit -m 'Version bump for publish script'
 make clean -C docs/
 make -C docs/
 rsync -avz docs/* chrism@mccormick.cx:~/mccormick.cx/projects/jsGameSoup/
+
+# push changes to Google Code
 bzr push https://mccormix@jsgamesoup.googlecode.com/svn/jsgamesoup/
+# push changes to the github repository
+bzr dpush git+ssh://git@github.com/chr15m/jsGameSoup.git
